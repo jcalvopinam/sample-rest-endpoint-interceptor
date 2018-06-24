c03b9fdc86d4c2152a7d6c3c841b8940db0fc39f
Juan Calvopina <juan.calvopina@gmail.com>
2018-06-24 19:54:42 -0300

    Rename the class and implement the use of Optional.ofNullable

--- src/main/java/com/jcalvopinam/interceptor/service/CustomRequestWrapper.java
+++ src/main/java/com/jcalvopinam/interceptor/service/CustomRequestWrapper.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018. JUAN CALVOPINA M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO CUSTOM_HEADER SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.jcalvopinam.interceptor.service;
 
 import javax.servlet.ReadListener;
@@ -9,14 +33,16 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Optional.ofNullable;
+
 /**
  * @author Juan Calvopina M. <juan.calvopina@gmail.com>
  */
-public class RequestWrapper extends HttpServletRequestWrapper {
+public class CustomRequestWrapper extends HttpServletRequestWrapper {
     private String body;
     private Map<String, String> headerMap;
 
-    public RequestWrapper(HttpServletRequest request) throws IOException {
+    public CustomRequestWrapper(HttpServletRequest request) throws IOException {
         super(request);
         this.setBody(request);
         this.setHeaders(request);
@@ -25,7 +51,7 @@ public class RequestWrapper extends HttpServletRequestWrapper {
     @Override
     public ServletInputStream getInputStream() {
         final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
-        ServletInputStream servletInputStream = new ServletInputStream() {
+        return new ServletInputStream() {
             @Override
             public boolean isFinished() {
                 return false;
@@ -45,7 +71,6 @@ public class RequestWrapper extends HttpServletRequestWrapper {
                 return byteArrayInputStream.read();
             }
         };
-        return servletInputStream;
     }
 
     @Override
@@ -62,7 +87,7 @@ public class RequestWrapper extends HttpServletRequestWrapper {
         BufferedReader bufferedReader = null;
         try {
             InputStream inputStream = request.getInputStream();
-            if (inputStream != null) {
+            if (ofNullable(inputStream).isPresent()) {
                 bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 char[] charBuffer = new char[128];
                 int bytesRead;
@@ -71,7 +96,7 @@ public class RequestWrapper extends HttpServletRequestWrapper {
                 }
             }
         } finally {
-            if (bufferedReader != null) {
+            if (ofNullable(bufferedReader).isPresent()) {
                 bufferedReader.close();
             }
         }
